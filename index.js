4134fca03932301feccb09db53b16f5ab2d2d305
cayan701 <cayan701@gmail.com>
2024-04-21 05:05:24 +0530

    Add new files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'test',
+    database: 'text_app',
     password: '24004486'
 });
 
