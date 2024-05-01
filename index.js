4ca4c87e7e6c84f254e28ee86f559d98da26a5ea
cayan701 <cayan701@gmail.com>
2024-05-01 05:31:56 +0530

    Add new files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require("path");
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "/views"));
